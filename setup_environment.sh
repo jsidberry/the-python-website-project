f76c9a8192ab1d58502df31f450055e642696bd0
Juan Sidberry <jsidberry@gmail.com>
2022-05-04 08:07:02 -0400

    add packages and environment configs

---
+++ setup_environment.sh
@@ -0,0 +1,13 @@
+# commands and notes to setup and use 
+# Flask Virtual Environments for Development
+
+python3 -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+# pip install -r requirements.txt
+set -o pipefail; pip install -r requirements.txt | { grep -v "already satisfied" || :; }
+
+export FLASK_APP=app.py
+export FLASK_ENV=development
+
+# flask run
